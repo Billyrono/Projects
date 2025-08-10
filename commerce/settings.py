3f06c518bf9aa5bd9452c90176da445effb61dec
Billy Rono <codespace@Rono.localdomain>
2025-08-10 23:47:30 +0300

    Fix static files settings for Render

--- commerce/settings.py
+++ commerce/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places Django will look for static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Whitenoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
